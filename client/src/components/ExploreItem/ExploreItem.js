d42e423f1ec4f428417700068ae8a419d1ce0424
Benjamin Shim <benjaminshim@Benjamins-MacBook-Pro.local>
2018-07-14 20:54:06 -0700

    Create base explore UI

---
+++ client/src/components/ExploreItem/ExploreItem.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+class ExploreItem extends Component {
+    render() {
+        return (
+            <article className="dt w-100 b--black-05 pb2 mt3 layers-item" href="#0">
+
+                <div className="dtc v-mid pl3">
+                    <h1 className="f6 f5-ns fw6 lh-title mv0">{this.props.name} </h1>
+                </div>
+
+                <div className="dtc v-mid tr">
+                    <img className="f6 w-10 h-10 b--black-10 dim pointer pv1 black-60" src={this.props.icon} />
+                </div>
+                
+            </article>
+        )
+    }
+}
+
+export default ExploreItem;
--- client/src/pages/Explore/Explore.js
+++ client/src/pages/Explore/Explore.js
@@ -1,16 +1,143 @@
 import React, {Component} from 'react';
 import Nav from '../../components/Nav';
 import './Explore.css';
+// import ExploreModal from '../../components/ExploreModal';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import ExploreItem from '../../components/ExploreItem';
+import layersList from '../Layers/layers.json';
+import exploreList from '../Explore/explore.json';
+
 
 class Explore extends Component {
+    
+    state = {
+        search: '',
+        location: '',
+        modal: false,
+        layersList,
+        exploreList
+    }
+
+    
+    handleInputChange = event => {
+        const {name, value} = event.target;
+        this.setState({
+            [name]: value
+        });
+    }
+
+    toggleModal = () => {
+        this.setState({ modal: !this.state.modal })
+    }
+
     render() {
         return (
             <div className="explore-body">
                 <Nav />
-                <h1>Explore</h1>
+                <div className="relative">
+                    <p className="tc" id="explore-header">Explore</p>
+                    <img  
+                        src='/assets/images/AstrolabIconImages/FilterMapImg.png'
+                        id="explore-filter"
+                        className="fr w2 h2 absolute top-0"
+                        onClick={this.toggleModal}/>
+                </div>
+
+                <div>
+                    <Modal 
+                        isOpen={this.state.modal} 
+                        toggle={this.toggleModal} 
+                        className={this.props.className}>
+                        <ModalHeader>
+                            Customize Explore
+                            <img  
+                                src='/assets/images/AstrolabIconImages/FilterMapActive.png'
+                                id="explore-filter-active"
+                                className="fr w2 h2 absolute top-0"
+                                onClick={this.toggleModal}/>
+                        </ModalHeader>
+                        <ModalBody>
+                            <div>
+                                <p>Slide to set search distance</p>
+                                <img 
+                                    src='/assets/images/AstrolabIconImages/ExploreSliderTrack.png'
+                                    id="explore-slider-track"
+                                    className="fr absolute w-75 top-4"/>
+                                <img 
+                                    src='/assets/images/AstrolabIconImages/ExploreSlider.png'
+                                    id="explore-slider"
+                                    className="fr absolute w2"/>
+                                <span 
+                                    id="explore-distance"
+                                    className="absolute">
+                                        <p>80 mi</p>
+                                </span>
+                            </div>
+
+                            <br />
+                            <br />
+
+                            <div>
+                                <p>Sort by</p>
+                                <button 
+                                    class="f6 link ph3 pv2 mb2 dib white bg-black" id="explore-btn-abc">Alphabetical</button>
+                                <button 
+                                    class="f6 link ph3 pv2 mb2 dib white bg-black" id="explore-btn-closest">Closest</button>
+                            </div>
+
+                            <br />
+
+                            <div>
+                                <p>Select layers to display</p>
+                                <p>Match map layers</p>
+                                <div>
+                                    {this.state.layersList.map(i => (
+                                        <button className="explore-layers-btn">{i.title}</button>
+                                    ))}
+                                </div>
+                            </div>
+                        </ModalBody>
+                    </Modal>
+                </div>
+
+
+
+                <form className="pa4 black-80">
+                    <div className="measure center">
+                        <input 
+                            id="explore-search" 
+                            className="input-reset ba b--white pa2 mb2 db w-100 br3"
+                            type="text" 
+                            placeholder="Search" 
+                            value={this.state.search}
+                            name="search"
+                            onChange={this.handleInputChange} />
+                        <br />
+                        <input 
+                            id="explore-location" 
+                            className="input-reset ba b--white pa2 mb2 db w-100 br3" type="text" 
+                            placeholder="Current Location" 
+                            value={this.state.location}
+                            name="location"
+                            onChange={this.handleInputChange} />
+                    </div>
+                </form>
+
+                <div>
+                    <main className="mw6 center">
+
+                        {this.state.exploreList.map(i => (
+                            <ExploreItem 
+                                name={i.name}
+                                icon={i.icon}/>
+                        ))}
+
+                    </main>
+                </div>
             </div>
         )
     }
 }
+
 
 export default Explore;
