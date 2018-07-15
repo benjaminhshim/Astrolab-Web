11151bc6725fd87eda54f49b6cced51f8c1c85f7
Benjamin Shim <benjaminshim@Benjamins-MacBook-Pro.local>
2018-07-14 18:41:30 -0700

    Create base Layers UI

--- client/src/pages/Layers/Layers.js
+++ client/src/pages/Layers/Layers.js
@@ -1,13 +1,30 @@
 import React, {Component} from 'react';
 import Nav from '../../components/Nav';
 import './Layers.css';
+import LayersItem from '../../components/LayersItem';
+import layersList from './layers.json';
 
 class Layers extends Component {
+    state = {
+        layersList
+    }
+
     render() {
         return (
             <div className="layers-body">
                 <Nav />
-                <h1>Layers</h1>
+                <p className="tc" id="layers-header">Layers</p>
+                <p className="tc" id="layers-subheader">Specify what you see</p>
+                <br />
+                <main className="mw6 center">
+
+                {this.state.layersList.map(i => (
+                    <LayersItem 
+                        title={i.title}
+                        icon={i.icon}/>
+                ))}
+
+                </main>
             </div>
         )
     }
