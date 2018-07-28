e99c0c9ead2019400f7e755603cffc5c468016cc
Celso Oliveira <celsooliveira@Celsos-MacBook-Air.local>
2018-07-28 11:21:17 -0700

    submit server file and models

---
+++ models/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+  Location: require("./bookmark"),
+  User: require("/.user")
+};
---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const PORT = process.env.PORT || 3001;
+const app = express();
+const routes = require("./routes");
+const mongoose = require("mongoose")
+// Define middleware here
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+// Use routes folder
+app.use(routes);
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/astrolab");
+
+
+
+app.listen(PORT, function() {
+  console.log(`🌎 ==> OUTKAST 🎧  ANDRE ${PORT}!`);
+});
